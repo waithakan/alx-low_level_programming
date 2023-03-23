e2091afe45005b207baf9013ea47a3766830fc43
Your Name <you@example.com>
2023-03-23 08:48:55 -0700

    function that checks for a digit (0 through 9).

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,13 @@
 /**
  * _isdigit - checks for a digit
  * @c: int to be checked
- * Return: 1 if c is a digit, otherwise if 0
+ * Return: 1 for chaaracter that will be a didgit or 0 for any else
  */
 
 int _isdigit(int c)
 
 {
-	
+
 	if (c >= 48 && c <= 57)
 	{
 	return (1);
